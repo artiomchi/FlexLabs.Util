594e09c350694dea005cb77852cc4620de071900
Artiom Chilaru <artiom@flexlabs.org>
2015-06-18 20:00:17 +0100

    + OrderByExpression tests

---
+++ test/FlexLabs.Util.Tests/Linq/OrderByExpressionTests.cs
@@ -0,0 +1,76 @@
+﻿using FlexLabs.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FlexLabs.Util.Tests.Linq
+{
+    public class OrderByExpressionTests
+    {
+        class ModelClass
+        {
+            public ModelClass(int id, string name)
+            {
+                ID = id;
+                Name = name;
+            }
+
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+        enum ModelSorter { ID, Name }
+
+        private static OrderByExpressionCollection<ModelSorter, ModelClass> _sorter = new OrderByExpressionCollection<ModelSorter, ModelClass>
+        {
+            { ModelSorter.ID, m => m.ID },
+            { ModelSorter.Name, m => m.Name },
+        };
+
+        private static List<ModelClass> _sample = new List<ModelClass>
+        {
+            new ModelClass(1, "5"),
+            new ModelClass(2, "4"),
+            new ModelClass(3, "3"),
+            new ModelClass(4, "2"),
+            new ModelClass(5, "1"),
+        };
+
+        [Fact]
+        public void OrderByExpression_OrderInt_Asc()
+        {
+            var sortedModel = _sorter[ModelSorter.ID].ApplyOrdering(_sample.AsQueryable(), true).ToList();
+            Assert.Equal(_sample.Count, sortedModel.Count);
+            Assert.Equal(1, sortedModel[0].ID);
+            Assert.Equal(5, sortedModel[4].ID);
+        }
+
+        [Fact]
+        public void OrderByExpression_OrderInt_Desc()
+        {
+            var sortedModel = _sorter[ModelSorter.ID].ApplyOrdering(_sample.AsQueryable(), false).ToList();
+            Assert.Equal(_sample.Count, sortedModel.Count);
+            Assert.Equal(5, sortedModel[0].ID);
+            Assert.Equal(1, sortedModel[4].ID);
+        }
+
+        [Fact]
+        public void OrderByExpression_OrderString_Asc()
+        {
+            var sortedModel = _sorter[ModelSorter.Name].ApplyOrdering(_sample.AsQueryable(), true).ToList();
+            Assert.Equal(_sample.Count, sortedModel.Count);
+            Assert.Equal(5, sortedModel[0].ID);
+            Assert.Equal(1, sortedModel[4].ID);
+        }
+
+        [Fact]
+        public void OrderByExpression_OrderString_Desc()
+        {
+            var sortedModel = _sorter[ModelSorter.Name].ApplyOrdering(_sample.AsQueryable(), false).ToList();
+            Assert.Equal(_sample.Count, sortedModel.Count);
+            Assert.Equal(1, sortedModel[0].ID);
+            Assert.Equal(5, sortedModel[4].ID);
+        }
+    }
+}
--- test/FlexLabs.Util.Tests/TypeConvertTests.cs
+++ test/FlexLabs.Util.Tests/TypeConvertTests.cs
@@ -3,8 +3,6 @@ using Xunit;
 
 namespace FlexLabs.Util.Tests
 {
-    // This project can output the Class library as a NuGet Package.
-    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
     public class TypeConvertTests
     {
         [Fact]
