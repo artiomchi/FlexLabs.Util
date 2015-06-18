e954856ed1c40d600fad2cb31d2b1030a9f84b88
Artiom Chilaru <artiom@flexlabs.org>
2015-06-18 10:02:20 +0100

    + OrderByExpression classes

---
+++ src/FlexLabs.Util/Linq/IOrderByExpression.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace FlexLabs.Linq
+{
+    public interface IOrderByExpression<T>
+    {
+        IOrderedQueryable<T> ApplyOrdering(IQueryable<T> query);
+        IOrderedQueryable<T> ApplyOrdering(IQueryable<T> query, bool ascending);
+    }
+}
